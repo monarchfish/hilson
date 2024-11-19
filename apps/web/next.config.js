1ec4ec264fd437d5381e104f7d60e6026c77abec
Susanna Wu <susannawcl1120@gmail.com>
2024-11-19 16:25:50 +0800

    feat: add svgr package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,24 +2,51 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
-
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   compiler: {
-    emotion: true,
+    emotion: true
   },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: false,
+    svgr: true
   },
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/ // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack']
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  }
 };
 
+// Compose the plugins for Next.js
 const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
+  // Add additional Next.js plugins here if needed
+  withNx
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
